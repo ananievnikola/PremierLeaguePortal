d5b22bdabdb78711b96555b1dc80f497ecc357d4
nananiev <nananiev@cleancodefactory.de>
2017-06-08 19:10:15 +0300

    more refactoring

---
+++ PremierLeaguePortal/Areas/Administration/Models/UserRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PremierLeaguePortal.Areas.Administration.Models
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+    }
+}
