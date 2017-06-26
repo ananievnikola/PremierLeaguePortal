9830f2deaece6f994bb65065129c7050638d258f
nananiev <nananiev@cleancodefactory.de>
2017-06-26 16:22:06 +0300

    ....

---
+++ PremierLeaguePortal.DAL/Models/VisualizationEnums/ELinkType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremierLeaguePortal.Models
+{
+    public enum ELinkType
+    {
+        CREATE = 0,
+        EDIT = 1,
+        DETAILS = 2,
+        PREVIEW = 3,
+        DELETE = 4
+    }
+}
