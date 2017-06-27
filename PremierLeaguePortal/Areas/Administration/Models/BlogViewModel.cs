8c17b9012de4d5385653070ae9b536b61eac977f
nananiev <nananiev@cleancodefactory.de>
2017-06-27 11:54:25 +0300

    tinyMCE added

--- PremierLeaguePortal/Areas/Administration/Models/BlogViewModel.cs
+++ PremierLeaguePortal/Areas/Administration/Models/BlogViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace PremierLeaguePortal.Areas.Administration.Models
 {
@@ -11,6 +12,7 @@ namespace PremierLeaguePortal.Areas.Administration.Models
         public int Id { get; set; }
         public string Header { get; set; }
         public string SubHeader { get; set; }
+        [AllowHtml]
         public string Content { get; set; }
         public EBlogCategory Category { get; set; }
         public Image HeaderImage { get; set; }
