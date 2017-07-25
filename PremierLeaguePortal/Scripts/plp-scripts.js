603c86c192fd871c288c133aae688379563c6e95
nananiev <nananiev@cleancodefactory.de>
2017-07-25 17:17:22 +0300

    facebook share button added

--- PremierLeaguePortal/Scripts/plp-scripts.js
+++ PremierLeaguePortal/Scripts/plp-scripts.js
@@ -1,13 +1,28 @@
-﻿//$('form input[type=submit]').click(function () {
-//    tinyMCE.triggerSave();
+﻿//$(document).ready(function () {
+//    console.log("ready!");
+//    tinymce.init({
+//        selector: 'textarea',
+//        oninit: "setPlainText",
+//        plugins: ["paste link"],
+//        
+//        //plugins: "link",
+//        toolbar: "link",
+//        default_link_target: "_blank"
+//    });
 //});
 
 $(document).ready(function () {
-    console.log("ready!");
     tinymce.init({
         selector: 'textarea',
+        entity_encoding: "raw",
         oninit: "setPlainText",
-        plugins: "paste"
+        plugins: [
+            'advlist autolink lists link image charmap preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars  fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+        ],
+        toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        image_advtab: true
     });
 });
 
